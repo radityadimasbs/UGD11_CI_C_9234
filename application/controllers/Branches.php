509f1790724bd98aff13fd48eb0ae474c30d09ea
radityadimasbs <55279013+radityadimasbs@users.noreply.github.com>
2019-11-14 12:22:42 +0700

    UGD 11 170709234

---
+++ application/controllers/Branches.php
@@ -0,0 +1,73 @@
+<?php use Restserver \Libraries\REST_Controller ; 
+Class Branches extends REST_Controller{ 
+    public function __construct(){     
+        header('Access-Control-Allow-Origin: *');     
+        header("Access-Control-Allow-Methods: GET, OPTIONS, POST, DELETE");         
+        header("Access-Control-Allow-Headers: Content-Type, ContentLength, Accept-Encoding");         
+        parent::__construct();         
+        $this->load->model('BranchesModel');
+        $this->load->library('form_validation');     
+    }     
+    public function index_get(){         
+        return $this->returnData($this->db->get('branches')->result(), false);     
+    }     
+    public function index_post($id = null){         
+        $validation = $this->form_validation;         
+        $rule = $this->BranchesModel->rules();         
+        if($id == null){ 
+            array_push($rule,[                     
+                'field' => 'name',              
+                'label' => 'name',             
+                'rules' => 'required|aplha'             
+            ],                 
+            [   
+                'field' => 'phoneNumber',   
+                'label' => 'phoneNumber',   
+                'rules' => 'required|is_unique[branches.phoneNumber]'     
+            ]             
+        );         
+    }
+    else{             
+        array_push($rule, 
+            [  
+                'field' => 'name',       
+                'label' => 'name',        
+                'rules' => 'required|aplha' 
+            ]  
+        ); 
+    }
+    $validation->set_rules($rule);        
+    if (!$validation->run()) {             
+        return $this->returnData($this->form_validation->error_array(), true); 
+    }         
+        $branches = new BranchesData();         
+        $branches->name = $this->post('name');         
+        $branches->address = $this->post('address');  
+        $branches->phoneNumber = $this->post('phoneNumber');    
+        $branches->created_at = $this->post('created_at');        
+        if($id == null){             
+            $response = $this->BranchesModel->store($branches); 
+        }else{             
+            $response = $this->BranchesModel->update($branches,$id);         
+        }         
+        return $this->returnData($response['msg'], $response['error']);     
+    }     
+    public function index_delete($id = null){         
+        if($id == null){             
+            return $this->returnData('Parameter Id Tidak Ditemukan', true);     
+        }        
+        $response = $this->BranchesModel->destroy($id);         
+        return $this->returnData($response['msg'], $response['error']); 
+    }     
+    public function returnData($msg,$error){         
+        $response['error']=$error;         
+        $response['message']=$msg;         
+        return $this->response($response);     
+    } 
+} 
+Class BranchesData{     
+    public $name;     
+    public $address;     
+    public $phoneNumber;
+    public $created_at; 
+}
---
+++ application/models/BranchesModel.php
@@ -0,0 +1,54 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
+ 
+class BranchesModel extends CI_Model {     
+    private $table = 'branches'; 
+ 
+    public $id;     
+    public $name;     
+    public $address;     
+    public $phoneNumber;
+    public $created_at;     
+    public $rule = [  
+        [             
+            'field' => 'name',             
+            'label' => 'name',             
+            'rules' => 'required'         
+        ],
+        [             
+            'field' => 'address',             
+            'label' => 'address',             
+            'rules' => 'required'         
+        ],     
+    ];    
+    public function Rules() { return $this->rule; }     
+ 
+    public function getAll() { return          
+        $this->db->get('data_mahasiswa')->result();      
+    }      
+    public function store($request) {          
+        $this->name = $request->name;          
+        $this->address = $request->address; 
+        $this->phoneNumber = $request->phoneNumber;    
+        $this->created_at = $request->created_at;          
+        if($this->db->insert($this->table, $this)){      
+            return ['msg'=>'Berhasil','error'=>false];
+        }         
+        return ['msg'=>'Gagal','error'=>true];     
+    }     
+    public function update($request,$id) {         
+        $updateData = ['address' => $request->address, 'name' =>$request->name, 'phoneNumber' =>$request->phoneNumber];         if($this->db->where('id',$id)->update($this->table, $updateData)){             
+                return ['msg'=>'Berhasil','error'=>false];         
+            }         
+        return ['msg'=>'Gagal','error'=>true];     
+    }     
+    public function destroy($id){         
+        if (empty($this->db->select('*')->where(array('id' => $id))->get($this->table)->row())) 
+            return ['msg'=>'Id tidak ditemukan','error'=>true];                  
+            
+        if($this->db->delete($this->table, array('id' => $id))){             
+            return ['msg'=>'Berhasil','error'=>false];     
+        }         
+        return ['msg'=>'Gagal','error'=>true];     
+    } 
+} 
+?> 
